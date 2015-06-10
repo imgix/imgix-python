17b9bb27d987a2118ed2a7a9855cb1d1ff52e823
Andy Gayton <andy@thecablelounge.com>
2015-06-10 12:35:59 -0700

    converts tabs to spaces

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import os
 import sys
 try:
-	from setuptools import setup
+    from setuptools import setup
 except:
-	from distutils.core import setup
+    from distutils.core import setup
 from distutils.command.build_py import build_py
 
 path, script = os.path.split(sys.argv[0])
@@ -14,12 +14,12 @@ VERSION="0.0.3"
 install_requires = []
 
 setup(name='imgix',
-	cmdclass={'build_py': build_py},
-	version=VERSION,
-	description='Python client library for imgix.',
-	author='imgix',
-	author_email='support@imgix.com',
-	url='http://www.imgix.com/',
-	packages=['imgix'],
-	install_requires=install_requires
+    cmdclass={'build_py': build_py},
+    version=VERSION,
+    description='Python client library for imgix.',
+    author='imgix',
+    author_email='support@imgix.com',
+    url='http://www.imgix.com/',
+    packages=['imgix'],
+    install_requires=install_requires
 )
