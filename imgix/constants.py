6af3a0874e8c7f7e5b0410fe300aad6fa3839a6e
sherwinski <sherwin@imgix.com>
2019-09-03 17:04:02 -0700

    fix: include dpr parameter when generating fixed-width srcset (#50)

--- imgix/constants.py
+++ imgix/constants.py
@@ -5,3 +5,23 @@ DOMAIN_PATTERN = re.compile(
             r'(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)'
             r'[a-z\d]{1,63}$'
         )
+SRCSET_INCREMENT_PERCENTAGE = 8
+SRCSET_MAX_SIZE = 8192
+
+
+def _target_widths():
+    resolutions = []
+    prev = 100
+
+    def ensure_even(n):
+        return 2 * round(n/2.0)
+
+    while prev <= SRCSET_MAX_SIZE:
+        resolutions.append(int(ensure_even(prev)))
+        prev *= 1 + (SRCSET_INCREMENT_PERCENTAGE / 100.0) * 2
+
+    resolutions.append(SRCSET_MAX_SIZE)
+    return resolutions
+
+
+SRCSET_TARGET_WIDTHS = _target_widths()
