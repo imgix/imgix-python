e5d31e1e8ac66637a4ec64647d91a752e6bb70fb
Eric Sanchez <esanchez@imgix.com>
2020-04-10 15:46:47 -0700

    refactor: srcset width tolerance

--- imgix/constants.py
+++ imgix/constants.py
@@ -5,13 +5,13 @@ DOMAIN_PATTERN = re.compile(
             r'(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)'
             r'[a-z\d]{1,63}$'
         )
-SRCSET_INCREMENT_PERCENTAGE = 8
-SRCSET_MAX_SIZE = 8192
 
-# The percentage by which image width-values are incremented by for
-# use in generating the default set of image widths, i.e.
-# `[IMAGE_MIN_WIDTH, .., IMAGE_MAX_WIDTH]`.
-INCREMENT_PERCENTAGE = 0.08
+# The srcset width tolerance dictates the _maximum tolerated size_
+# difference between an image's downloaded size and its rendered size.
+# For example, setting this value to 0.1 means that an image will not
+# render more than 10% larger or smaller than its native size.
+SRCSET_WIDTH_TOLERANCE = 8
+SRCSET_MAX_SIZE = 8192
 
 # Representation of an image with a width of zero. This value is used
 # in validation contexts, i.e. "is the width of the passed or requested
@@ -34,7 +34,7 @@ def _target_widths():
 
     while prev <= SRCSET_MAX_SIZE:
         resolutions.append(int(ensure_even(prev)))
-        prev *= 1 + (SRCSET_INCREMENT_PERCENTAGE / 100.0) * 2
+        prev *= 1 + (SRCSET_WIDTH_TOLERANCE / 100.0) * 2
 
     resolutions.append(SRCSET_MAX_SIZE)
     return resolutions
