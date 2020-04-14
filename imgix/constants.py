4b4ac4d8d89907f73ef125883099125d66cbbd70
eric <ericdeansanchez@berkeley.edu>
2020-04-14 12:50:32 -0700

    refactor: abstract validators (#61)
    * refactor: abstract validators
    
    This commit introduces `validators.py`. It isn't a feature, it's a
    utility. Abstracting validation out of individual files (where it
    makes sense to) helps us validate and test better.
    
    Instead of growing individual files with validation code,
    `validators.py` can grow. It can grow so much that eventually
    it could become
    ```text
    validators
    ├── v0.py
    └── v1.py
    ```
    if necessary.
    
    This commit is a part of my agenda to promote modularity and
    plug-ability. These validators can be used and reused,
    internally and externally.
    
    This commit also defines the dependencies it needs:
    ```
    IMAGE_ZERO_WIDTH = 0
    
    IMAGE_MIN_WIDTH = 100
    
    IMAGE_MAX_WIDTH = 8192
    ```
    They are additions to `SRCSET_MAX_SIZE` & `SRCSET_WIDTH_TOLERANCE`.
    I've added constants rather than remove/exchange to give us any and all
    time needed to deprecate ambiguously constants (if we choose).
    
    The purpose of defining these constants is to unify common values that
    are used throughout the codebase (e.g. the raw value `8192` is used
    three times in `test_srcset.py` alone).
    
    Note that the minimum value is _our_ minimum default value. Also note
    that while `IMAGE_ZERO_WIDTH` provides some benefit now, I'm confident
    it will pay dividends in the future.
    
    Each new validator is accompanied by a **group** of test functions
    located in `imgix-python/tests/test_validators.py`.
    
    This commit is in preparation for generating custom widths.
    
    - [x] **All tests pass**
    - [x ] CI Checks
    
    * refactor: fix flake8 error in last commit
    
    * refactor: srcset width tolerance
    
    * Update imgix/constants.py
    
    Co-Authored-By: sherwinski <sherwin@imgix.com>
    
    Co-authored-by: sherwinski <sherwin@imgix.com>

--- imgix/constants.py
+++ imgix/constants.py
@@ -5,8 +5,24 @@ DOMAIN_PATTERN = re.compile(
             r'(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)'
             r'[a-z\d]{1,63}$'
         )
-SRCSET_INCREMENT_PERCENTAGE = 8
+
+# The srcset width tolerance dictates the _maximum tolerated size_
+# difference between an image's downloaded size and its rendered size.
+# For example, setting this value to 0.1 means that an image will not
+# render more than 10% larger or smaller than its native size.
+SRCSET_WIDTH_TOLERANCE = 8
 SRCSET_MAX_SIZE = 8192
+
+# Representation of an image with a width of zero. This value is used
+# in validation contexts, i.e. "is the width of the passed or requested
+# image greater than or equal to the 'zero width image'."
+IMAGE_ZERO_WIDTH = 0
+
+# The minimum width of a default generated image-width.
+IMAGE_MIN_WIDTH = 100
+
+# The maximum width of a default generate image-width.
+IMAGE_MAX_WIDTH = 8192
 
 
 def _target_widths():
@@ -18,7 +34,7 @@ def _target_widths():
 
     while prev <= SRCSET_MAX_SIZE:
         resolutions.append(int(ensure_even(prev)))
-        prev *= 1 + (SRCSET_INCREMENT_PERCENTAGE / 100.0) * 2
+        prev *= 1 + (SRCSET_WIDTH_TOLERANCE / 100.0) * 2
 
     resolutions.append(SRCSET_MAX_SIZE)
     return resolutions
